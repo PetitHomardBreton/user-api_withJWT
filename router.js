714fdcb176eb798c629bb439cb7908c55ba80245
PetitHomardBreton <pierre-henri@barbe.contact>
2023-10-02 12:29:31 +0200

    fin du cours

--- router.js
+++ router.js
@@ -9,6 +9,24 @@ import updateUser from "./controllers/updateUser.js";
 import deleteUser from "./controllers/deleteUser.js";
 import login from "./controllers/login.js";
 
+function authenticateJWT(req, res, next) {
+    const token = req.header('Authorization');
+    
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication failed: No token provided' });
+    }
+    /* token= 'Bearer xxxx' */
+    jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (error, user) => {
+        if (error) {
+            return res.status(403).json({ message: 'Authentication failed: Invalid token' });
+        }
+        
+        req.user = user;
+        
+        next();
+    })
+}
+
 router.post('/users', createUser);
 router.get('/users', listUsers);
 router.get('/users/:id', getUser);
